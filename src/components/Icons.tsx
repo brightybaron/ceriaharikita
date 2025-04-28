f49c0f7b8b28a96dd829c4b4933ce43a2a110800
brightybaron <ardi.panda54@protonmail.com>
2025-04-28 21:09:47 +0700

    3rd push - perlu review

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -446,6 +446,41 @@ export const IconInstagram = () => (
   </svg>
 );
 
+export const IconFacebook = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+  </svg>
+);
+
+export const IconMusic = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="w-5 h-5"
+  >
+    <circle cx="8" cy="18" r="4" />
+    <path d="M12 18V2l7 4" />
+  </svg>
+);
+
 export const Icons = {
   IconMapPinned,
   IconMapPin,
@@ -471,6 +506,8 @@ export const Icons = {
   IconStar,
   IconMedic,
   IconInstagram,
+  IconFacebook,
+  IconMusic,
 };
 
 export default Icons;
